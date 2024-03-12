149e528186c64c6754273f8b38ddc3803fb0ad53
Elisa Castellari <elycaste03@gmail.com>
2024-03-12 15:56:34 +0100

    risolti bug e aggiunto supporto file. è in una nuove repository perchè avevo sviluppato da sola il codice in un'altra cartella

---
+++ structs.h
@@ -0,0 +1,22 @@
+/** \file structs.h
+	\brief defines the structs 
+	\author Paolo Gastaldo
+
+	Details.
+*/
+
+//#define _CRT_SECURE_NO_WARNINGS
+#ifndef STRUCTS_H 
+#define STRUCTS_H
+
+/*! \struct Poly
+	\brief stores the coefficients of a polynomial
+
+	coeffs[0] + coeffs[1]*x + coeffs[2]*x^2 + ......	
+*/
+typedef struct Poly {
+	double* coeffs; 	/* the coefficients, starting from the bias value  */
+	int degree; 	/* the number of elements in the array */
+} poly_s;
+
+#endif
